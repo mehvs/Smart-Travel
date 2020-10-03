6ea27926b25e4e6631266cb031e315c509dcba14
Цветослав Тодоров <gavril3vr@gmail.com>
2020-10-03 16:10:47 +0300

    reg

--- app/src/main/java/com/example/smarttravel/MainActivity.java
+++ app/src/main/java/com/example/smarttravel/MainActivity.java
@@ -2,16 +2,38 @@ package com.example.smarttravel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bsignUp;
+    Button bsingIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getViews();
+        setOnClickListeners();
     }
+
+    private void getViews() {
+        bsignUp = findViewById(R.id.bsignUp);
+        bsingIn = findViewById(R.id.bsignIn);
+
+    }
+
+    private void setOnClickListeners() {
+        bsignUp.setOnClickListener(v -> {
+
+            Intent registration = new Intent(this, Registration.class);
+            startActivity(registration);
+        });
+    }
+
 }
