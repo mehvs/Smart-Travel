c606a4c328d19c9dcef8ff3ad0f7094e91832115
HristoTsekov <xtsekooov@gmail.com>
2020-10-03 11:40:55 +0300

    first commit

---
+++ app/src/main/java/com/example/smarttravel/Login.java
@@ -0,0 +1,31 @@
+package com.example.smarttravel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Login extends AppCompatActivity {
+
+    private static final String VALID_EMAIL = "xtsekooov@gmail.com";
+    private static final String VALID_PASSWORD = "1914";
+
+    private EditText edtUsername;
+    private EditText edtPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        getViews();
+    }
+
+    private void getViews() {
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+    }
+}
