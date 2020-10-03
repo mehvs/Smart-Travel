743c677793a902801e1b480bf579c8fd8abf4fda
HristoTsekov <xtsekooov@gmail.com>
2020-10-03 20:59:44 +0300

    LogoSmartTravel

---
+++ app/src/main/java/com/example/smarttravel/SaveDestinatons.java
@@ -0,0 +1,14 @@
+package com.example.smarttravel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SaveDestinatons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_save_destinatons);
+    }
+}
